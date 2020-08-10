b055d5a20aa4235a5cf0b434f1f476a35552907e
esraaEmad <esso.em.96@gmail.com>
2020-08-10 04:40:05 +0200

    Updates

---
+++ precache-manifest.c0a515df1b834672ccecf0f8f4142f14.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef61cf71a4bc227bed87580eab38964",
+    "url": "/Remider_App_Reudx/index.html"
+  },
+  {
+    "revision": "85a94341bcb96561fe6e",
+    "url": "/Remider_App_Reudx/static/css/2.25ef79ed.chunk.css"
+  },
+  {
+    "revision": "4fc94f2de36a3c9defc6",
+    "url": "/Remider_App_Reudx/static/css/main.69fdfdbd.chunk.css"
+  },
+  {
+    "revision": "85a94341bcb96561fe6e",
+    "url": "/Remider_App_Reudx/static/js/2.d487fd7a.chunk.js"
+  },
+  {
+    "revision": "4fc94f2de36a3c9defc6",
+    "url": "/Remider_App_Reudx/static/js/main.de6f44de.chunk.js"
+  },
+  {
+    "revision": "4734aaf3f66affeed687",
+    "url": "/Remider_App_Reudx/static/js/runtime~main.167cde19.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Remider_App_Reudx/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Remider_App_Reudx/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Remider_App_Reudx/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Remider_App_Reudx/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Remider_App_Reudx/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Remider_App_Reudx/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Remider_App_Reudx/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Remider_App_Reudx/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Remider_App_Reudx/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Remider_App_Reudx/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Remider_App_Reudx/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Remider_App_Reudx/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Remider_App_Reudx/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Remider_App_Reudx/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Remider_App_Reudx/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "3917ebed7b95af38548167f854ca05fd",
+    "url": "/Remider_App_Reudx/static/media/logo.3917ebed.jpg"
+  }
+]);
